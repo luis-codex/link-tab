cd8b0bf9323844af84703429726c60102062f389
luis-tenorio-code <luisprograming0@gmail.com>
2024-08-06 10:30:02 -0500

    feat: Remove unused import in useAI.tsx

--- src/store/useAI.tsx
+++ src/store/useAI.tsx
@@ -1,4 +1,3 @@
-// import { createOpenAI as createGroq, OpenAIProvider } from '@ai-sdk/openai';
 import {
   GoogleGenerativeAIProvider,
   createGoogleGenerativeAI,
@@ -7,10 +6,6 @@ import storageChromeLocal from '@app/utils/storage';
 import { toast } from 'sonner';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-
-// AIzaSyAjudN-F8pqBOGrqke17JprkkReqJ-3Ni0 /* GOOGLE */
-// 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
-// AIzaSyDGO0BMUyeon1cmyfMnIdLdjNd2gIouo2Q
 
 type State = {
   apiKey: string | null;
